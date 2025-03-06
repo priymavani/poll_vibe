7f308e2b6c96484befc76f1fd5609da4f1011268
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-03-06 10:10:10 +0530

    Added poll_settings in Modal

--- Backend/Model/PollDetail.js
+++ Backend/Model/PollDetail.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
 
-
-
-
 const pollSchema = mongoose.Schema({
 
     poll_details: {
         poll_title: { type: String, required: true },
         poll_options: [{ type: String, required: true }],
-        oneVotePerIP: { type: String, required: true }
+    },
+
+    poll_settings: {
+        oneVotePerIP: { type: Boolean, required: true, default: true },
+        allowMultipleSelection: { type: Boolean, required: true, default: false },
+        requireParticipantName: { type: Boolean, required: true, default: false },
+        allowComments: { type: Boolean, required: true, default: false },
+        resultVisibility: { type: String, enum: ["Always Public", "Public after end Date", "Public after Vote", "Not Public"], required: true, default: "Always Public" },
+        closePollOnScheduleTime: { type: Boolean, required: true, default: false }
     },
 
     poll_result: {
@@ -17,11 +22,10 @@ const pollSchema = mongoose.Schema({
         ref: 'Result'
     },
 
-    Created_by: [{
+    Created_by: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-    }]
-
+    }
 
 }, { timestamps: true })
 
